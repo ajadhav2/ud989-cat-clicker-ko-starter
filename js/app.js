986db223c3f08a0da1a01f9fd0cb56570868f717
ashish53v <ashishjadhav53@gmail.com>
2017-07-13 04:51:45 -0400

    update files

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.level = ko.observable('Infant');
+	this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
+	this.imgAttribution = ko.observable('https://www.test.com');
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+		if(this.clickCount() >= 20){
+			// console.log("Adult");
+			this.level('Adult');
+		}else if(this.clickCount() >= 10){
+			// console.log("Teen");
+			this.level('Teen');
+		}
+	};
+}
+
+ko.applyBindings(new ViewModel());
